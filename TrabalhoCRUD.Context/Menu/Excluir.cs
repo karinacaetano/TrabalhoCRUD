61de44d4881449ca1b1713360f8eb280bb3da6cc
Karina Da Silva Caetano <karina.caetano@hbsis.com.br>
2020-08-20 11:06:56 -0300

    Adicionar arquivos de projeto.

---
+++ TrabalhoCRUD.Context/Menu/Excluir.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoCRUD.Context.Menu
+{
+    public class Excluir
+    {
+        public static void Executar(int tabela, int id)
+        {
+            if (tabela == 1)
+            {
+                ExcluirFuncionario(id);
+            }
+            else if (tabela == 2)
+            {
+                ExcluirPatrimonio(id);
+            }
+            else if (tabela == 3)
+            {
+                ExcluirCD(id);
+            }
+
+        }
+        public static void ExcluirFuncionario(int id)
+        {
+            var context = new DistribuicaoContext();
+
+
+            var funcionario = context.Funcionarios.FirstOrDefault(q => q.Id == id);
+            if (funcionario != null)
+            {
+                context.Funcionarios.Remove(funcionario);
+                Console.Write($"Funcionário {id} excluido com sucesso! ");
+            }
+
+
+            else
+                Console.WriteLine("Este código não existe");
+
+            context.SaveChanges();
+        }
+        public static void ExcluirPatrimonio(int id)
+        {
+            var context = new DistribuicaoContext();
+
+            var patrimonio = context.Patrimonios.FirstOrDefault(q => q.Id == id);
+            if (patrimonio != null)
+            {
+
+                context.Patrimonios.Remove(patrimonio);
+                Console.Write($"Patrimonio {id} excluido com sucesso! ");
+            }
+            else
+                Console.WriteLine("Este código não existe");
+
+            context.SaveChanges();
+        }
+        public static void ExcluirCD(int id)
+        {
+            Console.Write($"O que você deseja fazer com os funcionários e patrimonios associados ao codigo {id}?");
+            Console.Write($"1 - Para excluir \n2 - Para alterar \n..");
+            var excluir = Convert.ToInt32(Console.ReadLine());
+            if (excluir == 1)
+            {
+                var context = new DistribuicaoContext();
+
+                var cd = context.CentrosDistribuicao.FirstOrDefault(q => q.Id == id);
+                if (cd != null)
+                {
+                    var funcionario = context.Funcionarios.FirstOrDefault(q => q.Id == id);
+                    if (funcionario != null)
+                        context.Funcionarios.Remove(funcionario);
+                    var patrimonio = context.Patrimonios.FirstOrDefault(q => q.Id == id);
+                    if (patrimonio != null)
+                        context.Patrimonios.Remove(patrimonio);
+
+                    context.CentrosDistribuicao.Remove(cd);
+                    Console.Write($"Patrimonio {id} excluido com sucesso! ");
+                }
+                else
+                    Console.WriteLine("Este código não existe");
+                context.SaveChanges();
+            }
+            else if(excluir==2)
+            {
+                // escrever codigo de alterar aqui
+            }
+
+        }
+    }
+}
---
+++ TrabalhoCRUD/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrabalhoCRUD.Context;
+using TrabalhoCRUD.Context.Menu;
+
+namespace TrabalhoCRUD
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Digite o numero da tabela:  \n 1 - Funcionário \n 2 - Patrimônio \n 3 - Centro de Distribuição");
+            var tabela = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite o código que deseja excluir: ");
+            var id = Convert.ToInt32(Console.ReadLine());
+            Excluir.Executar(tabela, id);
+            
+        }
+    }
+}
